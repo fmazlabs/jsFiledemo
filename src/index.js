d782c56764124dfe33facdaed3330f1e2077104f
FMDEMOLABS <108448313+fmazlabs@users.noreply.github.com>
2022-07-16 10:19:43 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import setObjectPolyfill from './libs/polyfills/object';//demo16
+import setObjectPolyfill from './libs/polyfills/object';//demo17
 import setStringPolyfill from './libs/polyfills/string';
 import read from './read';
 import Document from './document/index';
